ea1c0942ac7b59f277eb7e96a8b110446231f15c
JohnCoene <jcoenep@gmail.com>
2020-07-21 10:57:04 +0200

    fix convenience funs + tests them

--- R/convenience.R
+++ R/convenience.R
@@ -5,17 +5,26 @@
 #' @param value Value to use.
 #' @param r,g,b,a Red, green, blue, and alpha values.
 #' 
+#' @section Functions:
+#' 
+#' * [important_()] - Makes it such that the change cannot be overwritten by other rules (other selections).
+#' * [rgb_()], [rgba_()] - Functions for red, green, blue and alpha for transparency.
+#' * [url_()] - Wrap in a `url` CSS function call.
+#' 
 #' @examples 
 #' Linne$
 #'  new()$
 #'  change(
 #'    sel_id("id"),
-#'    color = rgba_(255, 255, 255, .6)
+#'    color = rgba_(255, 255, 255, .6),
+#'    fontSize = important_(20)
 #'  )
 #' 
 #' @name convenience
 #' @export 
 important_ <- function(value){
+  if(inherits(value, "numeric") || inherits(value, "integer"))
+    value <- sprintf("%spx", value)
   sprintf("%s !important", value)
 }
 
@@ -34,5 +43,5 @@ rgb_ <- function(r, g, b){
 #' @rdname convenience
 #' @export 
 rgba_ <- function(r, g, b, a = .5){
-  sprintf("rgba(%s,%s,%s, %s)", r, g, b, a)
+  sprintf("rgba(%s,%s,%s,%s)", r, g, b, a)
 }
---
+++ tests/testthat/test-convenience.R
@@ -0,0 +1,8 @@
+context("Convenience")
+
+test_that("Basic", {
+  expect_equal(important_("this"), "this !important")
+  expect_equal(url_("https://john-coene.com"), "url(https://john-coene.com)")
+  expect_equal(rgb_(1,2,3), "rgb(1,2,3)")
+  expect_equal(rgba_(1,2,3, .5), "rgba(1,2,3,0.5)")
+})
